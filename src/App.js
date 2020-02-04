e775279ecff5e12b7a6761b18ff366f1f0d77d45
qrusadorz <qrusadorz@gmail.com>
2020-02-04 20:12:10 +0900

    add a button for execution

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import ProTip from './ProTip';
 
@@ -19,12 +20,20 @@ function Copyright() {
 }
 
 export default function App() {
+
+  function handleClick() {
+    console.log("click!");
+  }
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App v4-beta example
         </Typography>
+        <Button variant="contained" color="primary" fullWidth onClick={handleClick}>
+          try
+        </Button>
         <ProTip />
         <Copyright />
       </Box>
